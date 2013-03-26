42471275af78cd177146f99896651c5e38cdf629
dsmith110 <dsmith110@L11705038603.wctc.msft>
2013-03-26 17:28:06 -0500

    First GUI program

---
+++ src/firstgui/FirstGUI.java
@@ -0,0 +1,16 @@
+package firstgui;
+
+/**
+ * The first GUI using Java
+ * @author Dan Smith
+ * @version 1.00
+ */
+public class FirstGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
